466a4660e86db6c0c2af000f70711ca07530bbf9
lanae <rlfoard@gmail.com>
2018-06-14 15:13:01 -0700

    shuffle works

--- src/App.js
+++ src/App.js
@@ -46,13 +46,13 @@ class App extends Component {
       })
     }
     console.log(clickedArr);
-    // shuffle pics will get called here
+    this.shufflePics(pics);
   }
 
-  shuffleCards = cards => {
-    for (let i = cards.length - 1; i > 0; i--) {
+  shufflePics = pics => {
+    for (let i = pics.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [cards[i], cards[j]] = [cards[j], cards[i]];
+      [pics[i], pics[j]] = [pics[j], pics[i]];
     }
   }
 
